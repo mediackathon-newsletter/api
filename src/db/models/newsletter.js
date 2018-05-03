6239b29d6721add1f7a3d31dd704ab34f0a18892
HaFLiNGeR <jc.bailliez@gmail.com>
2018-05-03 19:09:19 +0200

    Many changes in Types / resolvers

--- src/db/models/newsletter.js
+++ src/db/models/newsletter.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const newsletterSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  journalist: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   city: { type: Schema.Types.ObjectId, ref: 'City', required: true },
   date: { type: Date, required: true },
   type: { type: String, enum: ['EVENTS', 'ARTICLES'], required: true }
+});
+
+newsletterSchema.virtual('articles', {
+  ref: 'Article',
+  localField: '_id',
+  foreignField: 'newsletter'
 });
 
 module.exports = mongoose.model('Newsletter', newsletterSchema);
